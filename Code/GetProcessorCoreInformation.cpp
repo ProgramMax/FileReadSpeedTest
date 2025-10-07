50c4c24a6b77c6762b4d29600ef33a113d51c565
Chris Blume <ProgramMax@gmail.com>
2025-10-07 10:41:40 -0400

    Add copyright notice

--- Code/GetProcessorCoreInformation.cpp
+++ Code/GetProcessorCoreInformation.cpp
@@ -1,3 +1,7 @@
+// Copyright 2025, The FileReadSpeedTest Contributors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "GetProcessorCoreInformation.hpp"
 
 #ifndef WIN32_LEAN_AND_MEAN
