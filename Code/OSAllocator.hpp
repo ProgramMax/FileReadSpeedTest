a629fe93ad87d08d6480ec6db8ddd014fb63fe59
Chris Blume <ProgramMax@gmail.com>
2025-10-07 10:41:57 -0400

    Add copyright notice (#14)

--- Code/OSAllocator.hpp
+++ Code/OSAllocator.hpp
@@ -1,3 +1,7 @@
+// Copyright 2025, The FileReadSpeedTest Contributors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef OSALLOCATOR_HPP
 #define OSALLOCATOR_HPP
 
