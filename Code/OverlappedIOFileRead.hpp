3a5a2f8c55e42503647143ca11fc1eaf2d9f87d4
Chris Blume <ProgramMax@gmail.com>
2025-10-07 10:39:39 -0400

    Add copyright notice

--- Code/OverlappedIOFileRead.hpp
+++ Code/OverlappedIOFileRead.hpp
@@ -1,3 +1,7 @@
+// Copyright 2025, The FileReadSpeedTest Contributors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef OVERLAPPEDIOFILEREAD_HPP
 #define OVERLAPPEDIOFILEREAD_HPP
 
